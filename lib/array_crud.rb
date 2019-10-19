903d2473b5e22624819babc7d5124361c5731e42
Ayako Wiltshire <awittyl44@gmail.com>
2019-10-19 05:47:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,8 @@ def create_an_array
   ["Jamel", "Jahki", "Elijah", "Melachi"]
 end
 
-def add_element_to_end_of_array[array, element]
+def add_element_to_end_of_array(array, element)
+  add_element_to_end_of_array = ["array", "element"]
   add_element_to_end_of_array << "shovel"
   p add_element_to_end_of_array
 end
